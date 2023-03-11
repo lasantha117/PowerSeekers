ae956641879c2e94cbf46efbaa2cb044d0b9535f
lasantha117 <weerasinghehwlp.20@uom.lk>
2023-03-11 13:58:12 +0530

    updated_relations between question and answers

---
+++ UG_WebApplication_frontend/src/app/question.service.ts
@@ -0,0 +1,20 @@
+// import { Injectable } from '@angular/core';
+// import {HttpClient} from "@angular/common/http";
+// import {Observable} from "rxjs";
+// import {object} from "underscore";
+//
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class QuestionService {
+//
+//   API = 'http://localhost:8080';
+//   constructor(private http : HttpClient) { }
+//
+//
+//     deleteQuestion(q_id?:string):Observable<object>{
+//     return this.http.delete('$');
+// }
+//
+//
+// }
